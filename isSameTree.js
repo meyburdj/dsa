943c14657aa342ccbb275ccb2b8c6ac6e036c063
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2023-04-11 12:53:47 -0700

    added docstring for isSameTree

--- isSameTree.js
+++ isSameTree.js
@@ -1,3 +1,10 @@
+/**
+ * Do DFS on p and q simultaneously. Base case is where either you reach the end
+ * and they've been the same the whole time, or where there is a time where they
+ * are different from eachother. Return the recursive left and right so that if either
+ * is false it returns false
+ */
+
 function isSameTree(p, q) {
     if (!p && !q) return true;
     if ((!p && q) || (p && !q)) return false;
